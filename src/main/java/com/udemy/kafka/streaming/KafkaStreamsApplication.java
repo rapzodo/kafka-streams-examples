5f5c9cae00369b0ffc86ba720cfc95102842770a
danilocastro <danilocastro81@gmail.com>
2021-03-10 13:59:30 -0600

    using properties builder with defaults

--- src/main/java/com/udemy/kafka/streaming/KafkaStreamsApplication.java
+++ src/main/java/com/udemy/kafka/streaming/KafkaStreamsApplication.java
@@ -1,12 +1,9 @@
 package com.udemy.kafka.streaming;
 
+import com.udemy.kafka.streaming.builder.KafkaPropertiesBuilder;
 import com.udemy.kafka.streaming.factory.TopologyFactory;
 import lombok.extern.log4j.Log4j2;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -42,17 +39,11 @@ public class KafkaStreamsApplication {
         log.info("running commandLineRunner");
 
         return args -> {
-            final Properties props = new Properties();
-            props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
-            props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-            props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-            props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-            props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
-            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, producerKeySer);
-            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, producerValueSer);
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, consumerKeyDeser);
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, consumerValueDeser);
-            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerAutoOffSetReset);
+            final Properties props = new KafkaPropertiesBuilder()
+                    .withApplicationId(applicationId)
+                    .withBootstrapServer(bootstrapServers)
+                    .withConsumerAutoOffsetReset(consumerAutoOffSetReset)
+                    .build();
 
             final KafkaStreams streams = new KafkaStreams(TopologyFactory.getWordsCountTopology(), props);
             streams.cleanUp(); // dont use in prod, this will delete localStore data related to application_id
